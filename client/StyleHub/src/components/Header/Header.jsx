5bedf5be0cb081d67211824817697ddb0de68281
VIvek Halder <vivekh.it.ug@jadavpuruniversity.in>
2024-04-30 12:42:42 +0530

    add the basic header

---
+++ client/StyleHub/src/components/Header/Header.jsx
@@ -0,0 +1,51 @@
+;import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+function Header() {
+  return (
+    <>
+        <div className='h-1/8 pb-3 bg-white flex' style={{borderBottom: '1px solid #ccc'}}>
+            <div className='text-3xl pr-7 pl-3'>
+                StyleHub
+            </div>
+            <div className='flex flex-grow text-xl'>
+                <ul className='flex h-full pl-6'>
+                    <li className='my-auto pr-5'>
+                        Category
+                    </li>
+                    <li className='my-auto pl-3 pr-5'>
+                        Collection
+                    </li>
+                    <li className='my-auto pl-3 pr-5'>
+                        Focus
+                    </li>
+                    <li className='my-auto pl-3 pr-5'>
+                        Service Point 1
+                    </li>
+                    <li className='my-auto pl-3 pr-5'>
+                        Assistance
+                    </li>
+                    <li className='my-auto pl-3 pr-5'>
+                        Contacts
+                    </li>
+                </ul>
+
+                <ul className='flex flex-grow justify-end mr-5'>
+                    <li className='my-auto pl-3 pl-5'>
+                        <FontAwesomeIcon icon={ faBell } />
+                    </li>
+                    <li className='my-auto pl-3 pl-5'>
+                        <FontAwesomeIcon icon={ faShoppingCart } />
+                    </li>
+                    <li className='my-auto pl-3 pl-5'>
+                        Account
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Header
